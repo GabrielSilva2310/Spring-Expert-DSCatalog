d074fa0f2d7c5ff5b704fe377c75d6f8ff4f3e5c
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-07-29 17:09:52 -0300

    First Repository test

--- src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTests.java
+++ src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTests.java
@@ -1,13 +1,31 @@
 package com.devsuperior.dscatalog;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@SpringBootTest
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+
+@DataJpaTest
 class DscatalogApplicationTests {
-
+	
+	@Autowired
+	private ProductRepository repository;
+	
 	@Test
-	void contextLoads() {
+	public void deleteShouldDeleteObjectWhenIdExists() {
+		Long existsId=5L;
+		
+		repository.deleteById(existsId);
+		Optional<Product> result=repository.findById(existsId);
+		
+		Assertions.assertFalse(result.isPresent());
+		
 	}
+	
 
 }
