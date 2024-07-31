df07cdf2fd4d0f2ca9b6f26dd69a41d48d764f30
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-07-31 18:45:06 -0300

    Test with finAllPaged of ProductService

--- src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -12,10 +12,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
@@ -63,6 +66,18 @@ public class ProductServiceTests {
 	}
 	
 	@Test
+	public void findAllShouldReturnPage() {
+		Pageable pageable=PageRequest.of(0, 10);
+		
+		Page<ProductDTO> result=service.findAllPaged(pageable);
+		
+		Assertions.assertNotNull(result);
+		
+		Mockito.verify(repository).findAll(pageable);
+		
+	}
+	
+	@Test
 	public void deleteShoudDoNothingWhenIdExist() {
 		
 		Assertions.assertDoesNotThrow(()->
@@ -91,6 +106,7 @@ public class ProductServiceTests {
 		});
 		
 	}
+	
 	
 	
 
