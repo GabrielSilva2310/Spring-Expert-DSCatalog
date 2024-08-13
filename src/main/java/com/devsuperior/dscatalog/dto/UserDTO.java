e07cd9298acba29e754dc737fc97a851d7c6cd87
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-08-13 00:09:08 -0300

    Basic beans validations

--- src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
+++ src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
@@ -6,13 +6,21 @@ import java.util.Set;
 
 import com.devsuperior.dscatalog.entities.User;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
 public class UserDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@NotBlank(message = "Campo Obrigatório!")
 	private String firstName;
 	private String lastName;
+	
+	@Email(message = "Favor entrar um email válido!")
 	private String email;
 	
 	private Set<RoleDTO> roles=new HashSet<>();
